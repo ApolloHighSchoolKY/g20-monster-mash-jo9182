7a110bfdbf0a1b744d190de0e1bf87097e51b5a5
jo9182 <73309930+jo9182@users.noreply.github.com>
2025-02-25 09:11:02 -0600

    Update MonsterMash.java

--- MonsterMash.java
+++ MonsterMash.java
@@ -1,5 +1,19 @@
 public class MonsterMash{
-  public static void main(String[] args){
-
-  }
+    public static void main(String[] args){
+        System.out.println("Welcome to Monster Mash");
+        
+        Monster creature = new Monster("Creature");
+        
+        System.out.println(creature);
+        System.out.println(creature.kill());
+        System.out.println(creature.getName());
+        
+        Monster west = new Witch("Wicked Witch of the West");
+        System.out.println(west);
+        System.out.println(west.kill());
+        
+        Monster mom = new Mother("Mom");
+        System.out.println(mom);
+        System.out.println(mom.kill());
+    }
 }
